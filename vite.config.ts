73a86d76373bde2588016756efd2988a18d478ee
ssinha102 <ssinha102@bloomberg.net>
2026-02-04 09:54:03 -0500

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Vital for GitHub Pages: uses relative paths for assets
-  base: './', 
+  base: './', // Critical for GitHub Pages relative paths
 });
